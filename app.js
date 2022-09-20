ad1868f2991747f2de5ee21bf04e5376e73addab
emanuel <emanuel01992@gmail.com>
2022-09-20 18:38:44 -0300

    cambios

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 
 let producto = [
-  { id: 1, producto: "huggies", precio: "2000$", imagen: "img/img.jpg" },
-  { id: 2, producto: "babysec", precio: "1200$", imagen: "img/img2.jpg" },
-  { id: 3, producto: "pampers", precio: "1000$", imagen: "img/img3.jpg" },
+  { id: 1, producto: "Huggies", precio: "2000$", imagen: "img/img.jpg" },
+  { id: 2, producto: "Babysec", precio: "1200$", imagen: "img/img2.jpg" },
+  { id: 3, producto: "Pampers", precio: "1000$", imagen: "img/img3.jpg" },
 ];
 
 
@@ -30,24 +30,29 @@ for (const objet of almasenados)
 const contenedor2 = document.querySelector(".divprueba1")
 for (const i of productos) {
 
-  
-  
-   
-
   const contenedor = document.createElement("div")
   contenedor.className = "divprueba"
   contenedor.innerHTML =
     `
     <div class="divcontenedor"> 
     <img class="img" src="${i.imagen}" alt="">
-    <h3 class="producto"> producto :  ${i.producto}<\h3>
-    <h2 > precio producto:</h2>
+    <h3 class="producto">${i.producto}<\h3>
+   
     <p class="precioproduc">  ${i.precio}</p> 
-    <input class="cantidadprod" type="number">
+   
      <button class="buton3" >enviar</button>
+     
      </div>
 `
+
   contenedor2.appendChild(contenedor)
+
+
+
+  // const capturadecambio= contenedor.querySelector('.cantidadprod')
+
+  // capturadecambio.addEventListener('change', capturadecambios);
+
 }
 
 // aca le hago que haga click a todos los botons 
@@ -61,44 +66,50 @@ boton.forEach((boton2) => {
 
 function clickdeboton(e) {
   const button = e.target;
-  
+
   // con el atributo closest ingreso al div por la clase y cuando oprimo el boton me envia el mas cercano 
   let boton2 = button.closest('.divprueba')
   let imgprins = boton2.querySelector(".img").src;
   let poductoss = boton2.querySelector(".producto").textContent;
   let precioprod = boton2.querySelector(".precioproduc").textContent;
-  let cantidad = boton2.querySelector(".cantidadprod").value
-  if (cantidad === "" || cantidad === "0"){
-   
-    swal({
-      title: "El campo esta vacio",
-      text: "AGREGE UNA CANTIDAD",
-      icon: "warning",
-      button: "aceptar",
-     
-    });
+  // let cantidad = boton2.querySelector(".cantidadprod").value
+
+
+
+
+  cArrito(imgprins, poductoss, precioprod);
+
+
+
+}
+
+// aca lo pego en el carrito
+// este lo traigo del html
+
+const divpruebaparcarriro = document.querySelector(".divprueba2")
+
+function cArrito(imgprins, poductoss, precioprod) {
+
+  const Noduplicarcarrito = divpruebaparcarriro.getElementsByClassName('producto');
+
+
+  for (let i = 0; i < Noduplicarcarrito.length; i++) {
+    Noduplicarcarrito[i].textContent
+    if (Noduplicarcarrito[i].textContent === poductoss) {
+      // aca con parentnode hago que al agregar no se dulique en el carrito el mismo elemento
+      // parent element si esta en el index y parent node si lo creamos con el dom
+      let noduplicar = Noduplicarcarrito[i].parentNode.parentNode.querySelector('.cantidadprod');
+      //  aca se suma la cantidad y y la funcion para sumar y return(retorne) para cortar y que se vuelva aejecutar
+      noduplicar.value++;
+      totalshopingcard()
+      return;
+
+    }
+
+
   }
 
 
- 
-  cArrito(imgprins, poductoss, cantidad, precioprod);
-  
-}
-
-
-
-// aca lo pego en el carrito
-const divpruebaparcarriro = document.querySelector(".divprueba2")
-
-function cArrito(imgprins, poductoss, cantidad, precioprod) {
-
-  // propiedad replace me permite sacar el signo de $ para tranformarlo en number y sumarlo
-  let tot = Number(precioprod.replace(
-    '$',
-    ''
-
-  ))
-  let totalparcial = tot * cantidad
   //  para multiplicar la cantidad por el precio y lo parseo
   // para acceder al boton de eliminar entramos a el div que lo contiene no a  document
   const divcarrrito = document.createElement("div")
@@ -106,56 +117,78 @@ function cArrito(imgprins, poductoss, cantidad, precioprod) {
 
   divcarrrito.innerHTML =
     `
-  <div class="divcontenedo"> 
+  <div class="divcontenedo">
+
+  
   <img class="img" src="${imgprins}" alt="">
-  <h3 class="producto"> ${poductoss}<\h3>
-  <input class="cantidadprod" type="number">
-  <h2 class="precioproductos">total: ${totalparcial}</h2>
+  <h3 id="producto" class="producto">${poductoss}</h3>
+  
+  <p class="precioproduc">${precioprod}</p> 
+ 
+  <input class="cantidadprod" type="number" value="1">
+  <p class="precioproductotal">   </p> 
    <button class="buton3" >enviar</button>
    <button class="butoneliminar" >eliminar</button>
    </div>
 `
 
-
-
-  // boton eliminminar 
-  // para acceder al boton de eliminar entramos a el div que lo contiene no a  document con queryselector
-  let botonelimina = divcarrrito.querySelector('.butoneliminar')
-  botonelimina.addEventListener('click', botoneliminar)
-  function botoneliminar(e) {
-    let clickcarrito = e.target
-
-    let clickcarritototal = clickcarrito.closest('.divcarrrito')
-
-
-  }
- 
-
-  // con el evento change entro al div contenedor y me captura cuando se realice un cambio dentro de ese div / o cualquier cosa 
-  let Sacaryponer = divcarrrito.querySelector('.cantidadprod')
-  Sacaryponer.addEventListener('change', capturacambio)
-  function capturacambio(e) {
-    let eliminacantidad = e.target
-
-    //  operadoor ternario
-    eliminacantidad.value <= 0 ? (eliminacantidad.value = 1) : null;
-let totaldecarrito = eliminacantidad.value * tot
-console.log(totaldecarrito)
-let preciotatlconcambios = divcarrrito.querySelector('.precioproductos')
- preciotatlconcambios.innerHTML=`${totaldecarrito}`
-// const totalCarrito = ca
-    // let cantidaprodCarrito = cantidadprod
-    
-
-
-
-  }
-
   divpruebaparcarriro.appendChild(divcarrrito)
+
+
+  // declaro la funcion para el boton eliminar y usarlo despues abajo 
+  divcarrrito.querySelector('.butoneliminar').addEventListener('click', botoneliminar)
+  divcarrrito.querySelector('.cantidadprod').addEventListener('change', botoncambiodecanmtidad)
+
+  totalshopingcard()
+
 
 }
 
- 
+
+function totalshopingcard() {
+  let total = 0;
+  let cardtotal = document.querySelectorAll('.divcarrito')
+  cardtotal.forEach(cardtotal => {
+    const totalprecio = cardtotal.querySelector('.precioproduc')
+
+    const cardetotalprecios = Number(totalprecio.textContent.replace(
+      '$',
+      ''
+    ))
+    const cantidadtotal = cardtotal.querySelector('.cantidadprod')
+    const cantidadtotalparse = Number(cantidadtotal.value)
+    total = total + cardetotalprecios * cantidadtotalparse
+
+
+
+  })
+
+
+  totaldelcarritodecompras.innerHTML = `${total}
+  <button value="enviar">enviar</button> `
+}
+
+const   totaldelcarritodecompras = document.querySelector('.divprueba3')
+
+
+
+
+// cons esto lo que hacemos en eliminar los productosdel carrito 
+function botoneliminar(e) {
+  const botoneliminar = e.target;
+  botoneliminar.closest('.divcarrito').remove();
+  // aca pego la funcion para que sume y reeste 
+  totalshopingcard()
+
+}
+// aca es la funciondee cambio de precio
+function botoncambiodecanmtidad(e) {
+  const cambiototal = e.target;
+  if (cambiototal.value <= 0) {
+    cambiototal.value = 1;
+  }
+  totalshopingcard()
+}
 
 
 
